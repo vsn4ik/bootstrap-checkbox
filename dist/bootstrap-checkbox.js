a4816cf5aa0f783f10346148e392fb1b7887b53b
vsn4ik <vsn4ik@gmail.com>
2014-01-18 16:14:26 +0400

    Update examples.

--- dist/bootstrap-checkbox.js
+++ dist/bootstrap-checkbox.js
@@ -15,7 +15,9 @@ if (typeof jQuery === 'undefined') {
 
 	var Checkboxpicker = function(element, options) {
 		this.element = element;
-		this.options = $.extend({}, $.fn.checkboxpicker.defaults, options, $(element).data());
+		this.$element = $(element).hide();
+
+		this.options = $.extend({}, $.fn.checkboxpicker.defaults, options, this.$element.data());
 
 		// .btn-group-justified only works with <a> elements as the <button> doesn't pick up the styles
 		this.$buttons = $('<a><a>').addClass('btn').click(this.clicked.bind(this));
@@ -23,38 +25,12 @@ if (typeof jQuery === 'undefined') {
 		this.$off = this.$buttons.eq(0).html(this.options.offLabel);
 		this.$on = this.$buttons.eq(1).html(this.options.onLabel);
 
-		this.init();
-
-		$(element).hide().change(this.render.bind(this));
-
 		this.$group = $('<div class="btn-group">')
 			.append(this.$buttons)
 			.keydown(this.keydown.bind(this))
 			.insertAfter(element);
 
-		if (this.options.style) {
-			this.$group.addClass(this.options.style);
-		}
-
-		if (element.title) {
-			this.$group.attr('title', element.title);
-		}
-		else {
-			if (this.options.offTitle) {
-				this.$off.attr('title', this.options.offTitle);
-			}
-
-			if (this.options.onTitle) {
-				this.$on.attr('title', this.options.onTitle);
-			}
-		}
-
-		if (element.disabled) {
-			this.$buttons.addClass('disabled');
-		}
-		else {
-			this.$group.attr('tabindex', 0);
-		}
+		this.init();
 	};
 
 	Checkboxpicker.prototype = {
@@ -67,6 +43,36 @@ if (typeof jQuery === 'undefined') {
 			}
 
 			this.$buttons.not('.active').addClass(this.options.defaultClass);
+
+			if (this.options.style) {
+				this.$group.addClass(this.options.style);
+			}
+
+			if (this.element.title) {
+				this.$group.attr('title', this.element.title);
+			}
+			else {
+				if (this.options.offTitle) {
+					this.$off.attr('title', this.options.offTitle);
+				}
+
+				if (this.options.onTitle) {
+					this.$on.attr('title', this.options.onTitle);
+				}
+			}
+
+			if (this.element.disabled) {
+				this.$buttons.addClass('disabled');
+			}
+			else {
+				this.$element.change(this.render.bind(this));
+
+				this.$group.attr('tabindex', 0);
+
+				if (this.element.autofocus) {
+					this.$group.focus();
+				}
+			}
 		},
 		render: function() {
 			this.$group.not(':focus').focus();
@@ -75,7 +81,7 @@ if (typeof jQuery === 'undefined') {
 			this.$on.toggleClass(this.options.onClass);
 		},
 		change: function() {
-			$(this.element).prop('checked', !this.element.checked).change();
+			this.$element.prop('checked', !this.element.checked).change();
 		},
 		clicked: function(event) {
 			if (!$(event.target).hasClass('active')) {
