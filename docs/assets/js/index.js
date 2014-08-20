50217728cf6bc12ff0fefb0e08b6a1cc7a746fdc
vsn4ik <vsn4ik@gmail.com>
2014-08-21 00:46:03 +0400

    Add scroll to top button.

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -4,4 +4,13 @@ $(function() {
 	$(':checkbox').checkboxpicker({
 		style: 'm-b'
 	});
+
+	$('#scroll_top').click(function() {
+		// 'html' for Mozilla Firefox, 'body' for other browsers
+		$('body, html').animate({
+			scrollTop: 0
+		}, 800);
+
+		this.blur();
+	});
 });
