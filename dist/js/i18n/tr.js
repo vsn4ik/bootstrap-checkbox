7dfcbb7189d2093b991d5bc2c9fd81a2d7720569
vsn4ik <vsn4ik@gmail.com>
2014-11-17 20:10:46 +0300

    Remove tabs.

--- dist/js/i18n/tr.js
+++ dist/js/i18n/tr.js
@@ -7,9 +7,9 @@
 'use strict';
 
 (function($) {
-	$.extend($.fn.checkboxpicker.defaults, {
-		offLabel: 'Pasif',
-		onLabel: 'Aktif',
-		warningMessage: 'Lütfen label elementi içerisinde Bootstrap-checkbox kullanmayınız.'
-	});
+  $.extend($.fn.checkboxpicker.defaults, {
+    offLabel: 'Pasif',
+    onLabel: 'Aktif',
+    warningMessage: 'Lütfen label elementi içerisinde Bootstrap-checkbox kullanmayınız.'
+  });
 })(jQuery);
--- js/i18n/tr.js
+++ js/i18n/tr.js
@@ -1,9 +1,9 @@
 'use strict';
 
 (function($) {
-	$.extend($.fn.checkboxpicker.defaults, {
-		offLabel: 'Pasif',
-		onLabel: 'Aktif',
-		warningMessage: 'Lütfen label elementi içerisinde Bootstrap-checkbox kullanmayınız.'
-	});
+  $.extend($.fn.checkboxpicker.defaults, {
+    offLabel: 'Pasif',
+    onLabel: 'Aktif',
+    warningMessage: 'Lütfen label elementi içerisinde Bootstrap-checkbox kullanmayınız.'
+  });
 })(jQuery);
