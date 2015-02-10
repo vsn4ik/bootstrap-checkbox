8c6b950d14eaca994d60773f36f47d79a0173824
vsn4ik <vsn4ik@gmail.com>
2015-02-10 10:10:03 +0300

    Upd grunt-contrib-cssmin.

--- js/i18n/es.js
+++ js/i18n/es.js
@@ -1,9 +1,3 @@
-/*!
- * Bootstrap-checkbox v1.2.4 (http://vsn4ik.github.io/bootstrap-checkbox)
- * Copyright 2013-2014 Vasily A. (https://github.com/vsn4ik)
- * Licensed under MIT (https://github.com/vsn4ik/bootstrap-checkbox/blob/master/LICENSE)
- */
-
 'use strict';
 
 (function($) {
