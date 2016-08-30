f98a7bb4091f879c2098ccc7bc7686684767de0d
vsn4ik <vsn4ik@gmail.com>
2016-08-31 01:30:54 +0300

    Fix docs octicons

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
         }, {
           expand: true,
           cwd: 'node_modules/octicons/build/font',
-          src: '*.{css,eot,svg,ttf,woff}',
+          src: ['*', '!*.scss'],
           dest: '_gh_pages/vendor/octicons/css'
         }]
       }
