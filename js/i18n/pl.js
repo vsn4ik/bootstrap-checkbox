ba5107c9f6727f4077b6f2e9bac7b05a1aedb3d5
Janusz <Eloar@users.noreply.github.com>
2016-10-21 11:17:22 +0000

    Update pl.js

--- js/i18n/pl.js
+++ js/i18n/pl.js
@@ -1,9 +1,9 @@
 'use strict';
 
 (function($) {
-    $.extend($.fn.checkboxpicker.defaults, {
-        offLabel: 'Nie',
-        onLabel: 'Tak',
-        warningMessage: 'Nie używaj elementu Bootstrap-checkbox wewnątrz elementu label.'
-    });
+  $.extend($.fn.checkboxpicker.defaults, {
+    offLabel: 'Nie',
+    onLabel: 'Tak',
+    warningMessage: 'Nie używaj elementu Bootstrap-checkbox wewnątrz elementu label.'
+  });
 })(jQuery);
