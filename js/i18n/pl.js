129e35657bd73a32dcb4d86db12abdbe914ec063
Janusz <Eloar@users.noreply.github.com>
2016-12-08 09:48:48 +0000

    Added Polish localisation
    Added js/i18n/pl.js file with Polish localization

---
+++ js/i18n/pl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function($) {
+  $.extend($.fn.checkboxpicker.defaults, {
+    offLabel: 'Nie',
+    onLabel: 'Tak',
+    warningMessage: 'Nie używaj elementu Bootstrap-checkbox wewnątrz elementu label.'
+  });
+})(jQuery);
