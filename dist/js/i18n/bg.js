d36b22b7d8dafbc9fd28f54bed6bc6c261f6e1a2
vsn4ik <vsn4ik@gmail.com>
2017-08-31 12:27:11 +0300

    dist

--- dist/js/i18n/bg.js
+++ dist/js/i18n/bg.js
@@ -1,3 +1,9 @@
+/*!
+ * Bootstrap-checkbox v1.4.0 (https://vsn4ik.github.io/bootstrap-checkbox/)
+ * Copyright 2013-2017 Vasily A. (https://github.com/vsn4ik)
+ * Licensed under the MIT license
+ */
+
 'use strict';
 
 (function($) {
